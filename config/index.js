9e509130b036d0b1c7f68b80b134187365d98d21
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-15 10:54:32 +0100

    route for handling form submissions for sending email

--- config/index.js
+++ config/index.js
@@ -2,5 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  "APP_PORT": process.env.APP_PORT
+  "APP_PORT": process.env.APP_PORT,
+  "MAIL_USER": process.env.MAIL_USER,
+  "MAIL_PASS": process.env.MAIL_PASS
 }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const nodemailer = require('nodemailer');
 const express = require('express');
 const app = express();
 const config = require("./config")
@@ -7,6 +8,36 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/send', (req, res) => {
+  const { email, message } = req.body;
+
+  if (!email || !message) {
+    return res.status(400).json({ message: "Please provide you info before you submit!." });
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: config.MAIL_USER,
+      pass: config.MAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: config.MAIL_USER,
+    subject: 'Contact me',
+    text: `Email: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    res.status(200).json(info);
+  });
 });
 
 const port = config.APP_PORT || 5000;
