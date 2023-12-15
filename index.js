76e006469d2991ce8c6d0ac805778cd856b1247c
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-15 10:40:36 +0100

    add config file to use APP_PORt env variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const config = require("./config")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const port = config.APP_PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
